a9f337e69ce65f29b562c0767549597a2f3b1470
Camarederic <camarederic@mail.ru>
2022-12-04 20:35:08 +0300

    Add Header to specific request

--- app/src/main/java/com/example/retrofit_57/api/SimpleApi.kt
+++ app/src/main/java/com/example/retrofit_57/api/SimpleApi.kt
@@ -6,6 +6,7 @@ import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,6 +14,10 @@ import retrofit2.http.QueryMap
 
 interface SimpleApi {
 
+    @Headers(
+        "Authorization: 123123123",
+        "Platform: Android"
+    )
     @GET("posts")
     suspend fun getPost(): Response<Post>
 
