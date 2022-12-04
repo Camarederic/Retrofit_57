a20c04ad48e469f54c4d73f0629c5c5fad438f2d
Camarederic <camarederic@mail.ru>
2022-12-04 12:28:55 +0300

    Create Multiple Queries

--- app/src/main/java/com/example/retrofit_57/repository/Repository.kt
+++ app/src/main/java/com/example/retrofit_57/repository/Repository.kt
@@ -10,11 +10,15 @@ class Repository {
         return RetrofitInstance.api.getPost()
     }
 
-    suspend fun getPost2(number: Int): Response<Post>{
+    suspend fun getPost2(number: Int): Response<Post> {
         return RetrofitInstance.api.getPost2(number)
     }
 
-    suspend fun getCustomPosts(userId: Int): Response<List<Post>>{
-        return RetrofitInstance.api.getCustomPost(userId)
+    suspend fun getCustomPosts(userId: Int, sort: String, order: String): Response<List<Post>> {
+        return RetrofitInstance.api.getCustomPost(userId, sort, order)
+    }
+
+    suspend fun getCustomPost2(userId: Int, options: Map<String,String>): Response<List<Post>>{
+        return RetrofitInstance.api.getCustomPost2(userId, options)
     }
 }
